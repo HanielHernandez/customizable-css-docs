43c5902f2206c56cf490fb5ea20b2030cc2852a5
Haniel <hhernandez@aimservices.tech>
2021-01-24 08:07:02 -0600

    home page and navbar added

---
+++ components/Header.vue
@@ -0,0 +1,5 @@
+<template lang="html">
+  <div class="navbar fixed navbar-collapse-md navbar-white">
+    <a class="brand">Brand</a>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,24 @@
 <template>
-  <div class="container"></div>
+  <div>
+    <div
+      class="row justify-center align-center"
+      style="height:calc(100vh - 57px);"
+    >
+      <div class="col col-w-12 col-w-md-8 col-w-lg-6 text-center">
+        <h1 class="font-7xl text-center">
+          Wellcome to Customizable css
+        </h1>
+        <p class="font-xl">
+          Customizable css is a css framework that use Utility based clasess as
+          well as Components
+        </p>
+        <div class="row justify-center">
+          <button class="btn btn-primary btn-size-4 ">Get started</button>
+          <button class="btn btn-primary-outline btn-size-4 ">Download</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
