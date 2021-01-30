f846adf05d34e302fc9c797e9f1805a9fb81dd8e
Haniel <hhernandez@aimservices.tech>
2021-01-29 18:44:19 -0600

    container minimize

--- components/Header.vue
+++ components/Header.vue
@@ -1,7 +1,20 @@
 <template lang="html">
   <div class="navbar fixed z-20 navbar-collapse-md navbar-white" style="top:0; right:0;">
+    <div class="container container-xxl row justify-between align-center" style="margin:0px auto;">
     <nuxt-link to="/" class="brand"> Customizable CSS</nuxt-link>
-    <button class="btn nav-toggler b-0" data-target="navbar_navigation">
+    <div class="searchbar-container display-md-none" :class="{show:showSearchBar}">
+      <search class="display-sm-0" />
+    </div>
+    <div class="pr-4">
+<a class="btn b-0 mr-0 p-2 display-sm-none " @click="toggleSearchBar()" data-target="navbar_navigation">
+      <i
+        class="material-icons nav-toggler-icon "
+        data-target="navbar_navigation"
+      >
+        {{showSearchBar?'close':'search'}}
+      </i>
+    </a>
+    <button class="btn nav-toggler p-2 mr-4 b-0" data-target="navbar_navigation">
       <i
         class="material-icons nav-toggler-icon"
         data-target="navbar_navigation"
@@ -9,9 +22,14 @@
         menu
       </i>
     </button>
-    <!--input type="text" class="form-input " style="width:150px" /-->
-    <div class="nav-menu justify-end hidden" id="navbar_navigation">
+    </div>
+
+    <!-- Navigation -->
+    <div class="nav-menu flex-auto justify-end hidden flex-auto" style="width:auto" id="navbar_navigation">
       <ul class="nav">
+        <li class="nav-item py-2 display-none display-md-block">
+                <search class="display-sm-0" />
+        </li>
         <li class="nav-item">
           <nuxt-link to="/" class="nav-link">Home</nuxt-link>
         </li>
@@ -22,6 +40,27 @@
           <nuxt-link to="/about" class="nav-link">About</nuxt-link>
         </li>
       </ul>
+       </div>
     </div>
   </div>
 </template>
+<script>
+import Search from "./Search";
+export default {
+  components: {
+    Search,
+  },
+  data: () => {
+    return {
+      showSearchBar: false,
+    };
+  },
+  methods: {
+    toggleSearchBar() {
+      this.showSearchBar = !this.showSearchBar;
+    },
+  },
+};
+</script>
+<style lang="scss">
+</style>
--- components/Search.vue
+++ components/Search.vue
@@ -1,24 +1,37 @@
 <template>
-  <ais-instant-search :search-client="searchClient" index-name="components">
+  <ais-instant-search :search-client="searchClient" index-name="docs">
         <ais-configure
           :attributesToSnippet="['bodyPlainText']"
           :hits-per-page.camel="5"
         >
-          <ais-autocomplete>
-            <div slot-scope="{ currentRefinement, indices, refine }" style="max-width: 300px; width:100%;" >
+          <ais-autocomplete v-click-outside="onClickOutside">
+            <div slot-scope="{ currentRefinement, indices, refine }" >
+              <div class="b r relative overflo-hidden">
+                <div class="absolute display-flex justify-center align-center" style="color:rgba(0,0,0,0.12);width:38px; height:38px; left:0; right:0;"  >
+                  <i class="material-icons " style="margin:0px auto;">
+                    search
+                  </i>
+                </div>
               <input
-                class="form-input"
+              class="input pl-9 py-2 r pr-4 w-12 b-0"
+              style="font-size:1rem"
                 type="search"
+                placeholder="Search"
                 :value="currentRefinement"
+                @focus="showResults = true"
                 @input="refine($event.currentTarget.value)"
                 autocomplete="off"
                 ref="searchInput"
               />
+              </div>
+              
               <div
-                class="absolute shadow-md r b"
-                v-show="currentRefinement.length"
+                class="absolute shadow-md r b z-40 bg-white " style="width:300px"
+                v-show="currentRefinement.length && showResults"
               >
                 <div v-for="section in indices" :key="section.objectID">
+                  <template v-if="section.hits.length>0">
+                  
                   <div class="h3 mb-0 p-4">
                     {{ section.indexName }}
                   </div>
@@ -31,7 +44,7 @@
                       v-for="hit in section.hits"
                       :key="hit.objectID"
                     >
-                      <nuxt-link :to="'#'" class="list-link w-12">
+                      <nuxt-link :to="'/docs/'+hit.category+'/'+hit.objectID" class="list-link w-12">
                         <div class="col">
                           <ais-highlight
                             attribute="title"
@@ -40,7 +53,7 @@
                           />
                           <ais-snippet
                             attribute="bodyPlainText"
-                            class="font-sm"
+                            class="font-sm truncate"
                             style="color: #5f5f5f"
                             :hit="hit"
                           />
@@ -48,11 +61,20 @@
                       </nuxt-link>
                     </li>
                   </ul>
+                     </template>
+                  <template v-if="section.hits.length==0">
+                    <div class="p-4">
+                      <p class="bold">No results found</p>
+                    </div>
+                     </template>
+
                 </div>
                 <ais-powered-by class="mb-4 px-4" />
               </div>
             </div>
           </ais-autocomplete>
+
+       
         </ais-configure>
   </ais-instant-search>
 </template>
@@ -60,16 +82,27 @@
 <script>
 import algoliasearch from "algoliasearch/lite";
 import "instantsearch.css/themes/algolia-min.css";
+import vClickOutside from 'v-click-outside'
 
 export default {
+   directives:{
+      clickOutside: vClickOutside.directive
+  },
   data() {
     return {
       searchClient: algoliasearch(
         process.env.ALGOLIA_APP_ID,
         process.env.ALGOLIA_API_KEY
       ),
+      showResults:false
     };
   },
+  
+  methods:{
+    onClickOutside(){
+      this.showResults = false;
+    }
+  }
 };
 </script>
 
@@ -79,5 +112,10 @@ export default {
 }
 .search-container {
   background: rgba(0, 0, 0, 25);
+}
+.input{
+  &:focus{
+    outline: none;
+  }
 }
 </style>
